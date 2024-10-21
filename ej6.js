442676e2c3f8169a6efc63cf46562acc98470511
Daniel <daniel.soto.ortiz@gmail.com>
2024-10-21 21:41:53 +0200

    fff

--- ej6.js
+++ ej6.js
@@ -2,15 +2,43 @@ window.onload = function(){
 
 
 
-function muestraMensaje(){
+    window.onload=function(){
 
-    let tinsertar = document.getElementById("tInsertar").value;
-    alert(tinsertar);
-}
+        document.getElementById("formulario1").addEventListener('submit',function(event){ 
+            event.preventDefault();//para que el formulario no se reenvie
+            const campodetexto = document.getElementById('campo1').value;
+            if (campodetexto) {
+    
+                const nuevoElemento = document.createElement('li');
+                nuevoElemento.textContent = campodetexto;
     
     
-    document.getElementById("insertar").onclick = muestraMensaje;
-    document.getElementById("eliminar").onclick = muestraMensaje;
-
-
-}
+                document.getElementById('lista').appendChild(nuevoElemento);
+    
+                document.getElementById('campo1').value='';
+    
+    
+                
+            }
+        }
+        );
+        
+        document.getElementById("formulario2").addEventListener('submit',function(event){ 
+            event.preventDefault();//para que el formulario no se reenvie
+            const campodetexto = document.getElementById('campo2').value;
+            if ( Number.isInteger(campodetexto) && document.getElementById('lista').children.length >0 ) {
+                let tabla =  document.getElementById('lista');
+                    tabla.removeChild(tabla.children[campodetexto -1])
+    
+                
+    
+    
+                
+            }
+    
+    
+    
+        });
+    
+    
+    }}
